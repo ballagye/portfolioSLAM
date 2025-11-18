1cea3569a58aa3bace20f51fb07ab633c9d3b702
ballagye <ballagyepro@gmail.com>
2025-11-18 20:45:49 +0100

    Svelte Init

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<div id="root">
+  <div id="App">
+    <div id="intro">
+      <div class="type-title">
+        <span class="intro-title">
+          Bonjour, Je m'appelle
+          <span class="intro-name">Balla</span>
+          .
+        </span>
+        <div class="intro-subtitle">I create stuff sometimes.</div>
+        <div class="intro-subtitle">Je suis étudiant à l'efrei blablabla</div>
+      </div>
+    </div>
+    <div class="section-header">
+      <span class="section-title"> / a propos</span>
+    </div>
+    <div class="about-content">
+      <div class="about-description">
+        <p></p>
+        text blablabla
+        <ul></ul>
+      </div>
+    </div>
+    <div id="experience">
+      <!-- Je m'en occupe plus tard -->
+      <div class="section-header">
+        <span class="section-title"> / éxpérience</span>
+      </div>
+    </div>
+    <div id="bts-sio">
+      <!-- Je m'en occupe plus tard -->
+      <div class="section-header">
+        <span class="section-title"> / BTS SIO</span>
+      </div>
+    </div>
+    <div id="projets">
+      <div class="section-header"></div>
+      <!-- Je m'en occupe plus tard -->
+    </div>
+  </div>
+</div>
+<!-- c'est root! -->
