a35046938253e3f37aeee61623c3c1aa0f4e288f
ballagye <ballagyepro@gmail.com>
2025-11-20 22:38:05 +0100

    few css changes (re design soon)

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -9,10 +9,9 @@
     <span class="intro-title">
       Bonjour, Je m'appelle
       <span class="intro-name">Balla</span>
-      .
     </span>
-    <span class="Blinking Cursor"></span>
-    <div class="intro-subtitle">I create stuff sometimes.</div>
+    <span class=" Cursor blinking-cursor"></span>
+    <div class="intro-subtitle">Je développe des applications.</div>
     <div class="intro-desc">Je suis étudiant à l'efrei blablabla</div>
   </div>
 </div>
@@ -25,8 +24,31 @@
     align-items: center;
     text-align: center;
     margin: auto;
-    max-width: 1000px;
+    max-width: 1500px;
     padding-top: 200px;
     min-height: 100vh;
+  }
+  .Typist {
+    font-size: 76px;
+    color: oklch(87% 0.065 274.039);
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+  }
+
+  .intro-name {
+    color: #6ecfff;
+  }
+  .intro-title {
+    font-weight: bold;
+  }
+  .intro-subtitle {
+    color: #858fad;
+    font-size: 80px;
   }
 </style>
