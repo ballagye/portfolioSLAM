2235adff2ce0657ceaeaaaea3616ccccef92c72b
ballagye <ballagyepro@gmail.com>
2025-11-23 23:01:07 +0100

    html for sisr section

--- src/lib/components/BtsSection.svelte
+++ src/lib/components/BtsSection.svelte
@@ -21,6 +21,33 @@
   <p>Le BTS SIO propose deux spécialités :</p>
 </div>
 
+<ul class="sisr-slam">
+  <li class="sisr-section">
+    <div class="sisr-content">
+      <div>
+        <p class="sisr-overline">SISR</p>
+        <h3 class="sisr-title">Réseaux</h3>
+      </div>
+      <div class="sisr-image"></div>
+    </div>
+  </li>
+  <div class="sisr-description">
+    <ul>
+      <li>Gestion des systèmes d’exploitation et des services réseaux</li>
+      <li>Gestion et la maintenance des infrastructures réseau</li>
+      <li>Optimisation des performances d’un réseau</li>
+      <li>Sécurisations des infrastructures</li>
+    </ul>
+  </div>
+  <div>
+    <ul class="sisr-tech-list">
+      <li>Machines Virtuelles</li>
+      <li>Linux</li>
+      <li>Windows Server</li>
+    </ul>
+  </div>
+</ul>
+
 <style>
   .bts-desc {
     display: flex;
@@ -34,5 +61,20 @@
     font-size: 400;
     color: oklch(96.2% 0.018 272.314);
     font-family: "SF Pro Display", sans-serif;
+  }
+  .sisr-description {
+    box-shadow: 0 10px 30px -15px var(--navy-shadow);
+    transition: var(--transition);
+    position: relative;
+    z-index: 2;
+    padding: 25px;
+    border-radius: var(--border-radius);
+    background-color: var(--indigo-dark);
+    color: var(--light-slate);
+    font-size: var(--fz-lg);
+  }
+  .sisr-content {
+    grid-column: 7 / -1;
+    text-align: right;
   }
 </style>
