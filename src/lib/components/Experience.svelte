9ac6573551e3390205b46a9cf440381a731404f2
ballagye <ballagyepro@gmail.com>
2025-11-28 23:31:46 +0100

    test(experience): testing buttons for tabs

--- src/lib/components/Experience.svelte
+++ src/lib/components/Experience.svelte
@@ -6,5 +6,8 @@
   <div class="section-header">
     <SectionHeader title={"/ Expérience"} />
   </div>
-  <div class="tab-list"></div>
+  <div class="tab-list">
+    <button class="tab-0"> Alstom</button>
+    <button class="tab-0"> SAIGON</button>
+  </div>
 </section>
