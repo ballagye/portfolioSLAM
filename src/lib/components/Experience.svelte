6715b1208b4b91c9e8e4580da1f19578469a98ce
ballagye <ballagyepro@gmail.com>
2025-11-30 20:32:47 +0100

    feat(experience): tabs css changes

--- src/lib/components/Experience.svelte
+++ src/lib/components/Experience.svelte
@@ -21,15 +21,14 @@
       </Tabs.List>
       <Tabs.Content value="alstom">
         <div class="alstom-content">
-          <h3 class="">
-            <span>Stage</span>
-            <span>
-              @<a href="https://www.alstom.com/fr" target="_blank">
-                Alstom</a
-              ></span
+          <h3>
+            <span>Stage DevOps</span>
+            <span class="at"> @ </span>
+            <a href="https://www.alstom.com/fr" target="_blank" class="company">
+              Alstom</a
             >
           </h3>
-          <p>Novembre 2025 - Décembre 2025</p>
+          <p>Mai 2025 - Juillet 2025</p>
           <ul>
             <li>foo</li>
             <li>foo</li>
@@ -42,9 +41,10 @@
         <div class="saigon-content">
           <h3>
             <span>Stage</span>
-            <span>@ <a href="blank"> SAIGON</a></span>
+            <span class="at"> @ </span>
+            <a href="" target="_blank" class="company"> SAIGON</a>
           </h3>
-          <p>Mai 2025 - Juillet 2025</p>
+          <p>Novembre 2025 - Décembre 2025</p>
           <ul>
             <li>foo</li>
             <li>foo</li>
@@ -63,5 +63,31 @@
     max-width: 1100px;
   }
   h3 {
+    font-family: var(--sfpro);
+    color: var(--light-indigo);
+    margin-bottom: 8px;
+    font-size: 20px;
+  }
+  .company {
+    position: relative;
+    text-decoration: none;
+    color: var(--blue);
+  }
+  .company:hover {
+    scale: 1.2;
+    text-decoration: underline;
+    text-decoration-thickness: 1px;
+    text-underline-offset: 4px;
+  }
+  .at {
+    text-decoration: none;
+    color: var(--blue);
+  }
+  p {
+    font-family: var(--ibm);
+    color: var(--light-indigo2);
+    font-size: 14px;
+    margin-bottom: 25px;
+    margin: 0px;
   }
 </style>
